8232a9cde2bfa6e8597dfeaabc47d8b3df1d4056
olepage@yeeti <olepage@yeeti.io>
2018-08-19 20:30:26 -0400

    Add Dropdown menu and menu items style + functionnality

---
+++ src/components/DropdownMenu/YeDropdown.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="dropdown-container" @mouseover="mouseOver" @mouseout="mouseOut">
+    <div class="dropdown-title">
+      {{ title }}
+    </div>
+    <ul :class="[dropdownClass, container]">
+      <YeDropdownItem v-if="menuItems"
+      v-for="item in menuItems"
+      :key="item.title"
+      :menuObject="item"/>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import YeDropdownItem from '@/components/DropdownMenu/YeDropdownItem'
+
+export default {
+  name: '',
+  components: { YeDropdownItem },
+  props: ['title', 'menuItems', 'styleObject', 'className'],
+  data () {
+    return {
+      dropdownClass: 'hidden',
+      container: 'container'
+    }
+  },
+  methods: {
+    mouseOver: function () {
+      this.dropdownClass = 'showing'
+    },
+    mouseOut: function () {
+      this.dropdownClass = 'hidden'
+    }
+  },
+  computed: {
+  },
+  watch: {},
+
+  /**************************
+  -> LIFECYCLE METHODS
+  **************************/
+
+  beforeCreate () {},
+  created () {},
+  beforeMount () {},
+  mounted () {},
+  beforeUpdate () {},
+  updated () {},
+  beforeDestroy () {},
+  destroyed () {}
+}
+</script>
+
+<style lang="css">
+
+  .dropdown-container {
+    display: inline-block;
+    position: relative;
+    box-sizing: border-box;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .dropdown-title {
+    display: inline-block;
+    padding: 10px 12px;
+    background-color: var(--secondary-color);
+    color: var(--main-color);
+  }
+
+  .container {
+    position: absolute;
+    top: 24px;
+    width: 300px;
+    background-color: var(--secondary-color);
+    box-shadow: 5px 5px 10px var(--secondary-color);
+    transition: opacity 0.15s;
+  }
+</style>
+<!--
+  WIKI:
+  menuItemObject: {
+    title: 'Products' // Displayed text of the menuItem,
+    type: 'href | router-link',
+    link: 'https://google.com', // [optional] if type == link
+    routerObj: { name: '', params: {} }
+  }
+ -->
---
+++ src/components/DropdownMenu/YeDropdownItem.vue
@@ -0,0 +1,71 @@
+<!--
+  has a link target or another menu target
+ -->
+<template v-if="menuObject">
+  <li v-if="menuObject.type === 'href'" class="item-container" @mouseover="mouseOver" @mouseout="mouseOut">
+    <a class="menu-item" :style="hoverStyle" :href="menuObject.link">{{ menuObject.title }}</a>
+  </li>
+
+  <li v-else-if="menuObject.type === 'router-link'" class="item-container" @mouseover="mouseOver" @mouseout="mouseOut">
+    <router-link class="menu-item" :style="this.hoverStyle" :to="menuObject.routerObj">{{ menuObject.title }}</router-link>
+  </li>
+</template>
+
+<script>
+// import Test from './something'
+export default {
+  name: 'YeDropdownItem',
+  components: {},
+  props: ['menuObject'],
+  data () {
+    return {
+      hoverStyle: {
+        backgroundColor: 'var(--secondary-color)'
+      }
+    }
+  },
+  methods: {
+    mouseOver: function () {
+      this.hoverStyle = {
+        backgroundColor: '#475472'
+      }
+    },
+    mouseOut: function () {
+      this.hoverStyle = {
+        backgroundColor: 'var(--secondary-color)'
+      }
+    }
+  },
+  computed: {},
+  watch: {},
+
+  /**************************
+  -> LIFECYCLE METHODS
+  **************************/
+
+  beforeCreate () {},
+  created () {},
+  beforeMount () {},
+  mounted () {},
+  beforeUpdate () {},
+  updated () {},
+  beforeDestroy () {},
+  destroyed () {}
+}
+</script>
+
+<style lang="css">
+  .menu-item {
+    display: block;
+    padding: 10px;
+    width: 100%;
+    height: 100%;
+    color: var(--main-color);
+    background-color: var(--secondary-color);
+    text-align: center;
+  }
+
+  .item-container {
+    width: 100%;
+  }
+</style>
