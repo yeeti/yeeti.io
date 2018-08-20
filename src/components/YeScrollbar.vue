649a1785706ef66835103c0e8fb775dced4f982a
olepage@yeeti <olepage@yeeti.io>
2018-08-19 21:38:36 -0400

    Add scrollbar component

---
+++ src/components/YeScrollbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="scrollbar" class="scrollbar-container" :style="barStyle" @scroll="onScroll">
+
+  </div>
+</template>
+
+<script>
+// import Test from './something'
+export default {
+  name: '',
+  components: {},
+  props: [],
+  data () {
+    return {
+      barStyle: {
+        height: '100px',
+        top: '10px'
+      }
+    }
+  },
+  methods: {
+    onScroll: function (event) {
+      console.log(event)
+    }
+  },
+  computed: {},
+  watch: {},
+
+  /**************************
+  -> LIFECYCLE METHODS
+  **************************/
+
+  beforeCreate () {},
+  created () {
+  },
+  beforeMount () {
+  },
+  mounted () {
+    this.$nextTick(function () {
+      let parent = this.$el.parentElement
+      parent.addEventListener('scroll', this.onScroll)
+      this.barStyle.height = (parent.clientHeight - 20) + 'px'
+    })
+  },
+  beforeUpdate () {},
+  updated () {
+  },
+  beforeDestroy () {},
+  destroyed () {}
+}
+</script>
+
+<style lang="css">
+
+  .scrollbar-container {
+    width: 10px;
+    position: absolute;
+    right: 5px;
+    background-color: black;
+  }
+</style>
--- src/components/YeTable.vue
+++ src/components/YeTable.vue
@@ -5,21 +5,24 @@
         <th class="header-cell" v-for="(value, key) in datasource[0]" :key="key">{{ key }}</th>
       </tr>
     </table>
-    <div class="table-container">
+    <div id="datatable" class="table-container">
       <table class="table">
         <tr class="row" v-for="row in datasource" :key="row.key">
           <td class="cell" v-for="cell in row" :key="cell.key">{{ cell }}</td>
         </tr>
       </table>
+      <YeScrollbar />
     </div>
   </div>
 </template>
 
 <script>
-// import Test from './something'
+
+import YeScrollbar from '@/components/YeScrollbar'
+
 export default {
   name: '',
-  components: {},
+  components: { YeScrollbar },
   props: {
     datasource: {
       type: Array,
@@ -40,7 +43,8 @@ export default {
   beforeCreate () {},
   created () {},
   beforeMount () {},
-  mounted () {},
+  mounted () {
+  },
   beforeUpdate () {},
   updated () {},
   beforeDestroy () {},
@@ -56,7 +60,8 @@ export default {
   }
 
   .table-container {
-    max-height: 300px;
+    position: relative;
+    height: 300px;
     width: 100%;
     box-sizing: content-box;
     overflow-y: scroll;
